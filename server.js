870038f716ab2b2c10dd4a54d7da317d0a7e1c23
Carston Work <carston.work@gmail.com>
2021-08-31 12:21:56 -0600

    made a small change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/student', (req, res) => {
-    const { name } = req.body
+    let { name } = req.body
     name = name.trim()
     students.push(name)
 
