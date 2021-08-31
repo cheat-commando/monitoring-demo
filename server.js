ccbbc3b52cb4d8f40ef351910da33e669384c945
Carston Work <carston.work@gmail.com>
2021-08-31 12:05:37 -0600

    Added more rollbar and functionality

--- server.js
+++ server.js
@@ -8,12 +8,26 @@ let rollbar = new Rollbar({
     captureUnhandledRejections: true
 })
 
+const students = []
+
 const app = express();
+app.use(rollbar.errorHandler())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
     rollbar.info('html file served successfully')
 });
+
+app.post('/api/student', (req, res) => {
+    const { name } = req.body
+    name = name.trim()
+    students.push(name)
+
+    rollbar.log('Student added sucessfully', {author: 'Carston', type: "manual entry"})
+
+
+    res.status(200).send(students)
+})
 
 const port = process.env.PORT || 4545;
 
